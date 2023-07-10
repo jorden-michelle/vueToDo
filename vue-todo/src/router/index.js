2f3cb00d835560783de77817087e7b8141de93d2
Jorden Ragelse <jordenragels3@gmail.comm>
2023-07-10 00:44:05 -0600

    to do input

--- vue-todo/src/router/index.js
+++ vue-todo/src/router/index.js
@@ -1,23 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ToDoView from '../views/ToDoView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import ToDoView from "../views/ToDoView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: ToDoView
+      path: "/",
+      name: "todo",
+      component: ToDoView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
--- vue-todo/src/views/ToDoView.vue
+++ vue-todo/src/views/ToDoView.vue
@@ -1,9 +1,38 @@
 <script setup>
+import { uid } from "uid";
+import { ref } from "vue";
+import ToDoCreator from "../components/ToDoCreator.vue";
 
+const todoList = ref([]);
+const createToDo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: false,
+    isEditing: null,
+  });
+};
 </script>
 
 <template>
   <main>
-   <h1>Hi Im Home</h1>
+    <h1>Create Todo</h1>
+    <ToDoCreator @create-todo="createToDo" />
   </main>
 </template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
