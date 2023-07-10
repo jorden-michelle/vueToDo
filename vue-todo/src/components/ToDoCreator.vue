ef52d0ea1e909186f0a898fea76cc70bb68286ee
Jorden Ragelse <jordenragels3@gmail.comm>
2023-07-10 00:59:17 -0600

    error message

--- vue-todo/src/components/ToDoCreator.vue
+++ vue-todo/src/components/ToDoCreator.vue
@@ -1,20 +1,32 @@
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 
 const emit = defineEmits(["create-todo"]);
 
-const todo = ref("");
+const todo = reactive({
+  todo: "",
+  invalid: false,
+  errMsg: "",
+});
 
-const createToDo = () => {
-    emit("create-todo", todo.value);
-}
+const createTodo = () => {
+  todo.invalid = false;
+  if (todo.todo !== "") {
+    emit("create-todo", todo.todo);
+    todo.todo = "";
+    return;
+  }
+  todo.invalid = true;
+  todo.errMsg = "I know you have something to do! Write Drink Water you dehydrated heathen";
+};
 </script>
 
 <template>
-  <div class="input-wrap">
-    <input type="text" v-model="todo" />
-    <button @click="createToDo">Create</button>
+  <div class="input-wrap" :class="{ 'input-err': todo.invalid }">
+    <input type="text" v-model="todo.todo" />
+    <button @click="createTodo()">Create</button>
   </div>
+  <p class="err-msg" v-if="todo.invalid">{{ todo.errMsg }}</p>
 </template>
 
 <style lang="scss" scoped>
@@ -22,6 +34,10 @@ const createToDo = () => {
   display: flex;
   transition: 250ms ease;
   border: 2px solid #41b080;
+
+  &.input-err {
+    border-color: red;
+  }
 
   &:focus-within {
     box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
@@ -42,5 +58,12 @@ const createToDo = () => {
     padding: 8px 16px;
     border: none;
   }
+}
+
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
